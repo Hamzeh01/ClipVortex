664d3eae4abfa0419c253aa2760bda8c99402746
Husen Hamze <summer.00.projects@gmail.com>
2023-10-11 22:45:37 +0330

    config tailwind colors and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screen: {
+      sm: "480px",
+      md: "768px",
+      lg: "992px",
+      xl: "1408px",
+    },
+    extend: {
+      color: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
